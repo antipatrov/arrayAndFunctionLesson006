146c96775d18042035dd99e8f12fb7ca344124a8
Yuri_Antipatrov <89209578005@yandex.ru>
2022-08-25 01:34:36 +0300

    Task041

---
+++ Task041/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("input numbers M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+int[] arrayM = new int[numberM];
+
+
+    for (int i = 0; i < numberM; i++)
+    {
+        arrayM[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+
+int countNegativeNumber = 0;
+
+ for (int i = 0; i < numberM; i++)
+ {
+    if (arrayM[i] < 0)
+    {
+        countNegativeNumber = countNegativeNumber + 1;       
+    }
+
+ }  
+System.Console.WriteLine($"{string.Join(",", arrayM)} => {countNegativeNumber}");
